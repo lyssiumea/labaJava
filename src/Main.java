7e28b4f7339bfc064e1bf730210853078c1af492
alyssa <lyssiumea@mail.ru>
2024-10-06 13:50:54 +0500

    1/20

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner; // для считывание ввода данных пользователя
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Methods methods = new Methods();
+        Scanner scanner = new Scanner(System.in);
+
+        // Сумма знаков
+        System.out.print("Введите число (не менее 2х знаков): ");
+        int number;
+        while (true) { // проверка на дурака
+            if (scanner.hasNextInt()){
+                number = scanner.nextInt();
+                if (Math.abs(number) >= 10 ){
+                    break;
+                } else {
+                    System.out.print("Число должно содержать минимум 2 цифры! Попробуйте снова: ");
+                }
+            } else {
+                System.out.print("Некорректный ввод. Введите целое число: ");
+                scanner.next();
+            }
         }
+        int result = methods.sumLastNums(number);
+        System.out.println("Сумма двух последних цифр: " + result);
+
+        scanner.close();
     }
 }
---
+++ src/Methods.java
@@ -0,0 +1,29 @@
+public class Methods { // Задание 1. Методы
+
+    // 2. Сумма знаков
+    public  int sumLastNums (int x) {
+        int lastNum = x % 10;
+        int secondLastNum = (x / 10) % 10;
+        return lastNum + secondLastNum; // возврат суммы 2 последних знаков числа x
+    }
+
+    // 4. Есть ли позитив
+//    public bool isPositive (int x) {
+//
+//    }
+
+    // 6. Большая буква
+//    public bool isUpperCase (char x) {
+//
+//    }
+
+    // 8. Делитель
+//    public bool isDivisor (int x, int b) {
+//
+//    }
+
+    // 10. Многократный вызов
+//    public int lastNumSum (int a, int b) {
+//
+//    }
+}
